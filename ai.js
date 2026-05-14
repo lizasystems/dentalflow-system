c9d5ee2c095604bd9991e9dc78891513ab0c7fd0
lizasystems <lizasystems@gmail.com>
2026-05-13 21:40:26 -0700

    Update ai.js

--- ai.js
+++ ai.js
@@ -23,7 +23,7 @@ exports.handler = async function(event, context) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-api-key': process.env.ANTHROPIC_API_key,
+        'x-api-key': process.env.ANTHROPIC_API_KEY,
         'anthropic-version': '2023-06-01'
       },
       body: JSON.stringify({
